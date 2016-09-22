8e0e394fc3e6b40c2c8c722f16e73e1c3c0d10cc
haoyuan <harryge00@gmail.com>
2016-09-22 19:54:50 +0800

    test k8s-svc

---
+++ k8s-service-stress-test/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "os"
+  "time"
+  "strconv"
+  "net/http"
+	"math/rand"
+	k8s_client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/api"
+)
+
+var svc api.Service
+func initSvc() {
+  svc.Spec.Type = api.ServiceTypeClusterIP
+  ports := make([]api.ServicePort, 2)
+  ports[0].Name = "echo"
+  ports[0].Port = 9998
+  ports[1].Name = "helloworld"
+  ports[1].Port = 9999
+  svc.Spec.Ports = ports
+  selector := make(map[string]string)
+  selector["k8s-app"] = "echo-server"
+  svc.Spec.Selector = selector
+}
+const letterBytes = "abcdefghijklmnopqrstuvwxyz"
+
+func RandStringBytes(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = letterBytes[rand.Intn(len(letterBytes))]
+    }
+    return string(b)
+}
+
+func main() {
+  rand.Seed(time.Now().UTC().UnixNano());
+	client, err := k8s_client.NewInCluster()
+	if err != nil {
+		fmt.Println("Failed to make client:")
+		panic(err)
+	}
+  initSvc()
+  svcNum, _ := strconv.Atoi(os.Getenv("SVC_NUMBER"))
+  svcNameMap := make(map[string]bool)
+  baseName := RandStringBytes(5)
+  fmt.Println(baseName)
+  for i := 0; i < svcNum; i++ {
+    svcName := "echo-" + baseName + strconv.Itoa(i)
+    svc.ObjectMeta.Name = svcName
+    fmt.Println(svc)
+    if _, err := client.Services("default").Create(&svc); err != nil {
+      //error when creating this svc
+      fmt.Println(err)
+      svcNameMap[svcName] = false
+      continue
+    }
+    svcNameMap[svcName] = true
+    if i % 10 == 0 {
+      for svcName := range svcNameMap {
+        if svcNameMap[svcName] {
+          //skip svc not created
+          url := "http://" + svcName + ":9998"
+          fmt.Println(url )
+          resp, err := http.Get(url)
+          if err != nil {
+              log.Fatal("error:", err)
+              continue
+          }
+          fmt.Println("response Status:", resp.Status)
+          fmt.Println("response Headers:", resp.Header)
+          fmt.Println("response Body:", resp.Body)
+          if resp.StatusCode != 204 {
+            fmt.Println("Error req")
+          }
+        }
+      }
+    }
+
+  }
+  select {
+
+  }
+}
